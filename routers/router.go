a1f513dd1d9e3fa45727c13d6c1416df4c439c59
kiwi520 <roczhmg@163.com>
2017-12-05 17:57:22 +0800

    optimize router

--- routers/router.go
+++ routers/router.go
@@ -9,10 +9,12 @@ import (
 )
 
 func init() {
-    //beego.Router("/", &controllers.MainController{})
 	beego.Router("/user", &controllers.UserController{})
 	beego.Router("/index", &index.IndexController{})
+
+	//百度编辑器ueditor
 	beego.Router("/ueidtor", &controllers.UeditorController{},"*:Ueditor")
+
 	admin :=
 		beego.NewNamespace("/admin",
 			beego.NSRouter("/", &admin.IndexController{}),
@@ -22,23 +24,18 @@ func init() {
 				beego.NSRouter("/add", &admin.ArticleController{}, "get:AddArticle;post:AddArticle"),
 				beego.NSRouter("/detail/:id", &admin.ArticleController{}, "get:GetDetailArticle"),
 				beego.NSRouter("/update/?:id", &admin.ArticleController{}, "get:UpdateArticle;post:UpdateArticle"),
-				//beego.NSRouter("/update/", &admin.ArticleController{}, "post:UpdateArticle"),
 			),
 		)
-	//注册 namespace
+	//注册 后台路由
 	beego.AddNamespace(admin)
-
-
-
 
 	api :=
 		beego.NewNamespace("/v1",
 			beego.NSNamespace("/article",
 				beego.NSRouter("/list", &apis.ArticleController{}, "get:ListArticle"),
 				beego.NSRouter("/detail/:id", &apis.ArticleController{}, "get:GetDetailArticle"),
-				//beego.NSRouter("/update/", &admin.ArticleController{}, "post:UpdateArticle"),
 			),
 		)
-	//注册 namespace
+	//注册 API路由
 	beego.AddNamespace(api)
 }
